d8904a338ddfce263c0fbde08a7cee7b7936a39d
brian <brian@death-star.quinnipiac.edu>
2018-11-05 12:45:16 -0500

    Power up appears and shrinks

--- Wave Game/src/mainGame/HealthPowerUp.java
+++ Wave Game/src/mainGame/HealthPowerUp.java
@@ -14,39 +14,38 @@ public class HealthPowerUp extends Pickup {
 	public HealthPowerUp(ID id, Handler handler) {
 		super((Game.WIDTH - 70)*Math.random(), (Game.HEIGHT - 120)*Math.random(), id);
 		this.handler = handler;
-		this.timer = 60;
-		this.sizeX = 50;
-		this.sizeY = 50;
+		this.timer = 10000;
+		this.sizeX = 100;
+		this.sizeY = 100;
 	}
 
 	@Override
 	public void tick() {
 		
 		if(timer > 0) {
-			this.updateSize();
+			if(timer % 10 == 0) {
+				this.updateSize();
+			}
 			timer --;
 		}
-		
-		
 	}
 
 	@Override
 	public void render(Graphics g) {
 		g.setColor(Color.PINK);
-		g.drawImage(img, (int) this.x, (int) this.y, 50, 50, null);
-		g.fillRect((int) this.x, (int) this.y, 50, 50);
+		g.drawImage(img, (int) this.x, (int) this.y, this.sizeX, this.sizeY, null);
+		g.fillRect((int) this.x, (int) this.y, this.sizeX, this.sizeY);
 		
 	}
 
 	@Override
 	public Rectangle getBounds() {
-		return new Rectangle((int) this.x, (int) this.y, 50, 50);
+		return new Rectangle((int) this.x, (int) this.y, this.sizeX, this.sizeY);
 	}
 	
 	public void updateSize() {
 		this.sizeX--;
 		this.sizeY--;
-
 		if (sizeX <= 0) {
 			handler.removePickup(this);
 		}
